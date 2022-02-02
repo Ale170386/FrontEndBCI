00e62a0e2b0e01522337fbbb23cd42f978e22897
Ale170386 <94206256+Ale170386@users.noreply.github.com>
2022-02-02 10:37:04 -0300

    Se modifica environment

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBackEnd: "http://40.87.62.201:81/",
+  apiBackEnd: "https://biclabs.eastus.cloudapp.azure.com/",
   apiBackEndKey: "51ac497a-a2fb-497b-be90-6eb14923f201"
 };
